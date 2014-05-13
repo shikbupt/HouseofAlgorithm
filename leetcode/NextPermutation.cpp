2f3b59d914ebd9818bc17a246b8bdfa3e3b82758
shikbupt <shi-kuan@qq.com>
2014-05-13 22:29:53 +0800

    finish Next Permutation

---
+++ leetcode/NextPermutation.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void nextPermutation(vector<int> &num) {
+        for (int i = num.size()-1; i >= 1; --i)
+        {
+            if (num[i] > num[i-1])
+            {
+                for (int j = num.size()-1; j >= i; --j)
+                {
+                    if (num[j] > num[i-1])
+                    {
+                        swap(num[j], num[i-1]);
+                        reverse(num.begin()+i, num.end());
+                        return;
+                    }
+                }               
+            }
+        }
+        reverse(num.begin(), num.end());
+        return;
+    }
+
+};
